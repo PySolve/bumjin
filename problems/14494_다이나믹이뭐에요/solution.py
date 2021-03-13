3b42df14b0556c0de1ca97705dbe3d7cac28ac94
Bumjin Park <bumjin@kaist.ac.kr>
2021-03-13 14:47:42 +0900

    solved

--- problems/14494_다이나믹이뭐에요/solution.py
+++ problems/14494_다이나믹이뭐에요/solution.py
@@ -1,16 +1,23 @@
+# === initialize dp ===
 n, m = map(int, input().split())
-MAX = max(n, m)
-dp = [[0 for i in range(MAX)] for j in range(MAX)]
-for i in range(MAX):
-    dp[0][i] =1 
-    dp[i][0] =1 
+dp = [[0 for i in range(n)] for j in range(m)]
+for i in range(m):
+    dp[i][0] =1
+for i in range(n):
+    dp[0][i] =1
 
-for i in range(1, MAX):
-    for j in range(1, i+1):
-        dp[j][i-j] += dp[j-1][i-j] + dp[j][i-j-1]
+# === fill the dp ===
+from collections import deque
+# fill the right side with bfs 
+queue = deque([[r,1] for r in range(m) if n>1])
+while queue:
+    r,c =queue.popleft() 
+    if r==0:
+        dp[r][c] =  dp[r][c-1] 
+    else:
+        dp[r][c] = dp[r-1][c] + dp[r][c-1] + dp[r-1][c-1]
+    if c+1<n:
+        queue.append([r, c+1])
 
-
-for d in dp:
-    print(d)
-
-        
+# === print the result ===
+print(dp[m-1][n-1]%1000000007)
