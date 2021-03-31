106f141fa23840a7d6f21db85d6b59c42ab8739d
Bumjin Park <bumjin@kaist.ac.kr>
2021-03-31 19:58:46 +0900

    udpate

--- problems/15992_1,2,3더하기7/solution.py
+++ problems/15992_1,2,3더하기7/solution.py
@@ -1,4 +1,4 @@
-dp = [[0 for i in range(1001)] for j in range(1001)]
+dp = {j:{} for j in range(1001)}
 dp[1][1] = 1
 dp[2][1] = 1
 dp[3][1] = 1 
@@ -6,14 +6,11 @@ dp[3][1] = 1
 def solve(n,m):
     if m<1 or n<1: # basis
         return 0
-
-    if dp[n][m] >0: # overapping 
+    if m in dp[n].keys():
         return dp[n][m]
     else:  # optimal substructure
         dp[n][m] = solve(n-3, m-1) + solve(n-2, m-1) + solve(n-1, m-1)
         return dp[n][m]
-
-
 
 import sys 
 sys.setrecursionlimit(1000000)
--- problems/17144_미세먼지안녕/solution.py
+++ problems/17144_미세먼지안녕/solution.py
@@ -1,5 +1,88 @@
 
 
 R, C, T = map(int, input().split())
-import sys 
-board = [list(map(int, sys.stdin.readline().split()) for i in range(R)]
+import sys
+import copy  
+board1 = [list(map(int, sys.stdin.readline().split())) for i in range(R)]
+board2 = copy.deepcopy(board1)
+
+
+air_position = []
+for i in range(R):
+    for j in range(C):
+        if board1[i][j] == -1:
+            air_position.append([i,j])
+
+
+dr = [0,0,1,-1]
+dc = [1,-1, 0,0]
+
+def spread(t):
+    global R, C
+    if t%2==0:
+        board = board1
+        other = board2 
+    else:
+        board = board2 
+        other = board1 
+    
+    for i in range(R):
+        for j in range(C):
+            if other[i][j] != -1:
+                other[i][j] = 0
+
+    for i in range(R):
+        for j in range(C):
+            if board[i][j] != -1:
+                value = board[i][j]
+                spread = value//5 
+                spreaded = 0
+                for d in range(4):
+                    nr, nc = i + dr[d], j + dc[d]
+                    if 0<= nr < R and 0 <= nc < C:
+                        if other[nr][nc] != -1:
+                            other[nr][nc] += spread 
+                            spreaded += spread 
+                other[i][j] += value - spreaded 
+    return other 
+
+def run_machine(board):
+    x,y = air_position[0]
+    for r in range(x-2,-1, -1):
+        board[r+1][0] = board[r][0]
+    for c in range(1, C):
+        board[0][c-1] = board[0][c]
+    for r in range(1, x+1):
+        board[r-1][-1] =  board[r][-1]
+    for c in range(C-2, 0, -1):
+        board[x][c+1] = board[x][c]
+    board[x][1] = 0 
+
+
+    x,y  = air_position[1]
+    for r in range(x+2, R):
+        board[r-1][0] = board[r][0]
+    for c in range(1, C):
+        board[-1][c-1] =board[-1][c]
+    for r in range(R-2, x-1, -1):
+        board[r+1][-1] = board[r][-1]
+    for c in range(C-2, 0, -1):
+        board[x][c+1] = board[x][c]  
+    board[x][1] = 0 
+
+    return board
+
+
+for i in range(T):
+    board = spread(i)
+    board = run_machine(board)
+
+
+
+count = 0
+for i in range(R):
+    for j in range(C):
+        if board[i][j]>0 : 
+            count += board[i][j]
+
+print(count)
